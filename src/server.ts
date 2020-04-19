1d3d7437eb2b738251f0c39d6d79073fb423b198
andre.ribeiro <andre.ribeiro@iv2.com.br>
2020-04-19 13:52:17 -0300

    eslint

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.use(
-  (error: Error, request: Request, response: Response, next: NextFunction) => {
+  (error: Error, request: Request, response: Response, _: NextFunction) => {
     if (error instanceof AppError) {
       return response.status(error.statusCode).json({
         status: 'error',
