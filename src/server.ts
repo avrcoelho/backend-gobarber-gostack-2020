4de856ed98325567ab432ed4e61f69821f9f4ae7
andre.ribeiro <andre.ribeiro@iv2.com.br>
2020-04-19 13:28:07 -0300

    route from files

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import 'reflect-metadata';
 import express from 'express';
 
+import uploadConfig from './config/upload';
 import routes from './routes';
 import './database';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
